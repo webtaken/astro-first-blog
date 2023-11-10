211caf45f1ab1bdbfdfe074728afdb092a2bb5b6
saul <luckly083@gmail.com>
2023-11-10 11:45:14 -0500

    finished the project!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
   adapter: vercel(),
   site: "https://astro-first-blog.vercel.app/",
+  integrations: [preact()]
 });
